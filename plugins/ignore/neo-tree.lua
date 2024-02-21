50fc35e715bb32e63d0b9b3208a7261f0824748e
RickLiu <rickliu@glenfly.com>
2024-02-21 11:34:04 +0800

    revert open terminal(origin repo implement) add grep in folder

--- plugins/ignore/neo-tree.lua
+++ plugins/ignore/neo-tree.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
-        opts.commands.copy_path = function(state)
+        opts.commands.open_terminal = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
             path = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h")
@@ -9,9 +9,13 @@ return {
             -- 设置寄存器
             local register = '*'
             vim.cmd('let @' .. register .. ' = ' .. vim.fn.string(path))
-            require('telescope.builtin').live_grep({search_dirs = {path}})
+            
+            -- 构建打开终端命令并执行
+            local myCommand = "ToggleTerm  dir=" .. path
+            local commandString = "vim.cmd[[" .. myCommand .. "]]"
+            load(commandString)()
         end
         
-        opts.window.mappings.L = "copy_path & grep in"
+        opts.window.mappings.T = "open_terminal"
     end
 }
