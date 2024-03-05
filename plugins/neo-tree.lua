6ff5265cff1e4fe4aaad99140db38ea6b38b7c95
RickLiu <rickliu@glenfly.com>
2024-03-05 20:13:30 +0800

    remote

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -18,18 +18,23 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
+      "miversen33/netman.nvim",
     },
     opts = function(_, opts)
+
+
         opts.commands.copy_locate_path = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
             path = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h")
 		path = path..'/'
 	    vim.fn.setreg('l', path)
-								require("user.select-dir").save_unique_string(path)
+	vim.g.base_search_dir = path
+								require("user.select-dir").save_unique_string(vim.g.base_search_dir)
 								vim.notify(path,'info', {
   									title = "base seach dir",
       							}) 
+		
 	    path = vim.fn.fnamemodify(path, ":~:.:h")
             
             -- 设置寄存器
@@ -54,6 +59,38 @@ return {
         opts.window.mappings.L = "copy_locate_path"
 	opts.window.mappings.G = "grep_in_path"
 	opts.window.mappings["<leader>r"] = "spectre"
-	
+
+	      opts.window.mappings.uu = {
+        function(state)
+          vim.cmd("TransferUpload " .. state.tree:get_node().path)
+        end,
+        desc = "upload file or directory",
+        nowait = true,
+      }
+
+      -- download (sync files)
+      opts.window.mappings.ud = {
+        function(state)
+          vim.cmd("TransferDownload" .. state.tree:get_node().path)
+        end,
+        desc = "download file or directory",
+        nowait = true,
+      }
+
+      -- diff directory with remote
+     opts.window.mappings.uf = {
+        function(state)
+          local node = state.tree:get_node()
+          local context_dir = node.path
+          if node.type ~= "directory" then
+            -- if not a directory
+            -- one level up
+            context_dir = context_dir:gsub("/[^/]*$", "")
+          end
+          vim.cmd("TransferDirDiff " .. context_dir)
+          vim.cmd("Neotree close")
+        end,
+        desc = "diff with remote",
+      }
     end
 }
